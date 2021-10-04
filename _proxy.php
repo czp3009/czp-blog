49de259a46426b6016bc9845eb74256b6ecbe4bc
czp3009 <czp3009@gmail.com>
2021-10-04 15:44:28 +0800

    use http proxy for gitalk proxy

---
+++ _proxy.php
@@ -0,0 +1,3 @@
+<?php
+$proxy = 'tcp://localhost:10809';
+?>
--- gitalk_proxy.php
+++ gitalk_proxy.php
@@ -1,17 +1,21 @@
 <?php
+require 'proxy.php';
 if($_SERVER['REQUEST_METHOD'] != 'POST') {
     die();
 }
 $requestBody = file_get_contents('php://input');
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/json\r\n",
-        'method'  => 'POST',
-        'content' => $requestBody
-    )
+$httpArgs = array(
+    'header'  => "Content-type: application/json\r\n",
+    'method'  => 'POST',
+    'content' => $requestBody
 );
-$context  = stream_context_create($options);
-$result = file_get_contents("https://github.com/login/oauth/access_token", false, $context);
+if(isset($proxy)) {
+    $httpArgs['proxy'] = $proxy;
+}
+$options = array(
+    'http' => $httpArgs
+);
+$result = file_get_contents("https://github.com/login/oauth/access_token", false, stream_context_create($options));
 if ($result === FALSE) {
     http_response_code(504);
     die();
